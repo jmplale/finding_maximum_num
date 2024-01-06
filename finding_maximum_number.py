a2ab438b097f063c811f5b383e813af339fbad65
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-06 17:20:57 +0800

    completed inputs

--- finding_maximum_number.py
+++ finding_maximum_number.py
@@ -1,3 +1,5 @@
 # we need to make inputs for the user.
 
-num_one = int(("What is your first number?: "))
+num_one = int(input("What is your first number?: "))
+num_two = int(input("What is your second number?: "))
+num_three = int(input("What is your third number?: "))
