e617a4e20ccb3e085ae473e5f89e587864db3023
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-06 23:45:05 +0800

    adding def main to start designing the code's outcome

--- finding_maximum_number.py
+++ finding_maximum_number.py
@@ -15,19 +15,22 @@ def colored_letter(text, color):
 
 # we need to make inputs for the user.
 
-num_one = int(input("What is your first number?: "))
-num_two = int(input("What is your second number?: "))
-num_three = int(input("What is your third number?: "))
+def main():  # putting def main to put some design to the code
+    num_one = int(input("What is your first number?: "))
+    num_two = int(input("What is your second number?: "))
+    num_three = int(input("What is your third number?: "))
    
-# I just fixed some few errors
-# we need to make if statement for num_one
-if num_one > num_two and num_one > num_three:
-    print("\nThe largest number among the three numbers you've entered is " + str(num_one))
-else:
-    # also an if statement for num_two 
-    if num_two > num_one and num_two > num_three:
-        print(("\nThe largest number among the three numbers you've entered is " + str(num_two)))
-
-    # the else statement for num_three
+    # I just fixed some few errors
+    # we need to make if statement for num_one
+    if num_one > num_two and num_one > num_three:
+        print("\nThe largest number among the three numbers you've entered is " + str(num_one))
     else:
-        print(("\nThe largest number among the three numbers you've entered is " + str(num_three)))
+        # also an if statement for num_two 
+        if num_two > num_one and num_two > num_three:
+            print(("\nThe largest number among the three numbers you've entered is " + str(num_two)))
+
+        # the else statement for num_three
+        else:
+            print(("\nThe largest number among the three numbers you've entered is " + str(num_three)))
+
+            
