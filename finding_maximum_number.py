44e5c4994c2ced5d5203228e828fee357b18fac5
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-06 17:25:46 +0800

    plan/scratch for if-else statements

--- finding_maximum_number.py
+++ finding_maximum_number.py
@@ -3,3 +3,9 @@
 num_one = int(input("What is your first number?: "))
 num_two = int(input("What is your second number?: "))
 num_three = int(input("What is your third number?: "))
+
+# we need to make if statement for num_one
+
+# also an if statement for num_two 
+
+# the else statement for num_three
