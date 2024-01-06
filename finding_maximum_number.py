c706cda84828e9554fcc953986fd1c0055199e9a
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-06 17:31:29 +0800

    if statement for num_two

--- finding_maximum_number.py
+++ finding_maximum_number.py
@@ -7,6 +7,9 @@ num_three = int(input("What is your third number?: "))
 # we need to make if statement for num_one
 if num_one > num_two and num_one > num_three:
     print("\nThe largest number among the three numbers you've entered is " + str(num_one))
-# also an if statement for num_two 
 
+# also an if statement for num_two 
+if num_two > num_one and num_two > num_three:
+    print(print("\nThe largest number among the three numbers you've entered is " + str(num_two)))
+    
 # the else statement for num_three
